3652ebf8e0288a00b7d6355bf1b36a19a6a3765d
Stéphane Nédélec <misterned72@gmail.com>
2019-04-23 07:10:20 +0200

    deux urls

--- venv/Scripts/pb/pb/urls.py
+++ venv/Scripts/pb/pb/urls.py
@@ -18,4 +18,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', admin.site.urls),
+    path('users/<name:str>/detail/', admin.site.urls),
 ]
