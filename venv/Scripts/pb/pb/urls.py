d2f4e25f57b43d2f81e47bdd9f24331512644ac1
Stéphane Nédélec <misterned72@gmail.com>
2019-04-23 08:48:46 +0200

    Views et urls

--- venv/Scripts/pb/pb/urls.py
+++ venv/Scripts/pb/pb/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from user import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', admin.site.urls),
-    path('users/<name:str>/detail/', admin.site.urls),
+    path('users/', views.hello),
+    path('users/<str:name>/detail/', views.user_detail),
 ]
--- venv/Scripts/pb/user/views.py
+++ venv/Scripts/pb/user/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def hello(request):
+    return HttpResponse('Hello PB')
+
+def user_detail(request, name):
+    body = "Nom : {}".format(name)
+    return HttpResponse(body)
