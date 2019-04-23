f8df94067e81746bd3a9f74edb535da845822ed5
Stéphane Nédélec <misterned72@gmail.com>
2019-04-23 06:21:53 +0200

    initialisation du dépot

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\nedel\onedrive\docume~1\stphan~1\boulot\docume~1\python\django\robinp~1\pb\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
